3d637466f805c31690a400ba071aa5742d8a439f
cdbelizaire <carmelajmb@brandeis.edu>
2015-11-06 12:01:55 -0500

    Added professors info

--- client/professors.js
+++ client/professors.js
@@ -1,13 +1,32 @@
 var professors = [
 	{
-		name:"",
-		office_location:"",
-		office_hours:"Wed 10:00-14:00, Fri 12:00-15:00",
-		typ_courses:"",
-		website_url:"",
+		name:"Sarah Lupis",
+		office_location:"Brown Rabb 117",
+		office_hours:"Fri 12:00-2:00 and by appointment",
+		typ_courses:"Quantitative Reasonings",
+		website_url:"https://moodle2.brandeis.edu/syllabus/default/153TYP-2A-1_1440522485.pdf",
 	},
-	{},
-	{}
+	{
+		name:"Craig Smith",
+		office_location:"Rabb 323",
+		office_hours:"M/W 3-4",
+		typ_courses:"Social Science",
+		website_url: "https://moodle2.brandeis.edu/syllabus/default/153TYP-5A-1_1444410588.pdf"
+	},
+	{
+		name:"Tim Hickey",
+		office_location:"Rabb 323",
+		office_hours:"By appointment",
+		typ_courses:"Computer Science",
+		website_url: "https://sites.google.com/a/brandeis.edu/typ-cs-aut15/home"
+	},
+	{
+		name:"Sarah Sutton ",
+		office_location:"",
+		office_hours:"By appointment",
+		typ_courses:"Writing 1",
+		website_url: ""
+    }
 ];
 Template.professors.helpers({
 	professors: function(){
