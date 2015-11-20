f42bc6b466d03c973ddafd9ae52155d23c711f89
Quayowens <quay@brandeis.edu>
2015-11-20 13:01:15 -0500

    I changed the style of the MKTYP Classes and added a professor to the MKTYP Professors

--- client/classes.js
+++ client/classes.js
@@ -1,12 +1,48 @@
 var classes = [
+{
+		name:"Quantitative Reasoning 1",
+		class_location:"Goldsmith 317", 
+		class_hours:"M/W/Th 10:00 - 10:50",
+		instructor:"Joseph Martin",
+	},
+
 	{
 		name:"Quantitative Reasoning 2",
 		class_location:"Farber 101",
 		class_hours:"M/W/Th 12:00 - 12:50",
 		instructor:"Sarah Lupis",
-		course_url:"https://moodle2.brandeis.edu/syllabus/default/153TYP-2A-1_1440522485.pdf",
+	},
+
+	{
+		name:"Writing 1",
+		class_location:"Shiffman 201", 
+		class_hours:"Tu/Th 3:30 - 4:50",
+		instructor:"Sarah Sutton",
+	},
+
+	{
+		name:"Writing 2",
+		class_location:"Brown Social Science Center 322a", 
+		class_hours:"Tu/Th 6:30 - 7:50",
+		instructor:"Doug Bafford",
+	},
+
+	{
+		name:"Social Science",
+		class_location:"Shiffman 201", 
+		class_hours:"Tu/Th 2:00 - 3:20",
+		instructor:"Craig Smith",
 	},
 	
+	{
+		name:"Computer Science",
+		class_location:"Farber 101", 
+		class_hours:"Tu/F 12:30 - 1:50",
+		instructor:"Tim Hickey",
+	},
+
+	
+
 ];
 Template.classes.helpers({
 	classes: function(){
--- client/professors.js
+++ client/professors.js
@@ -1,12 +1,40 @@
 var professors = [
+{
+		name:"Joseph Martin",
+		office_location:"Goldsmith 317",
+		office_hours:"Wed 11:00-12:30 and by appointment",
+		typ_course:"Quantitative Reasoning 1",
+		course_url:"",
+		email:"j0sephmartin@brandeis.edu"
+	},
+
 	{
 		name:"Sarah Lupis",
 		office_location:"Brown Rabb 117",
 		office_hours:"Fri 12:00-2:00 and by appointment",
-		typ_course:"Quantitative Reasonings",
+		typ_course:"Quantitative Reasoning 2",
 		course_url:"https://moodle2.brandeis.edu/syllabus/default/153TYP-2A-1_1440522485.pdf",
 		email:"slupis@brandeis.edu"
 	},
+
+	{
+		name:"Sarah Sutton ",
+		office_location:"",
+		office_hours:"By appointment",
+		typ_course:"Writing 1",
+		course_url: "",
+		email:"ssutton@brandeis.edu"
+    },
+
+    {
+		name:"Doug Bafford",
+		office_location:"Brown Social Science Center, Room 218",
+		office_hours:":Mon 4:30-5:30,Fri 10:00-11:00, and by appointment",
+		typ_course:"Writing 2",
+		course_url: "",
+		email:"bafford@brandeis.edu"
+    },
+
 	{
 		name:"Craig Smith",
 		office_location:"Rabb 323",
@@ -23,22 +51,8 @@ var professors = [
 		course_url: "https://sites.google.com/a/brandeis.edu/typ-cs-aut15/home",
 		email:"tjhickey@brandeis.edu"
 	},
-	{
-		name:"Sarah Sutton ",
-		office_location:"",
-		office_hours:"By appointment",
-		typ_course:"Writing 1",
-		course_url: "",
-		email:"ssutton@brandeis.edu"
-    },
-    {
-		name:"Doug Bafford",
-		office_location:"Brown Social Science Center, Room 218",
-		office_hours:":Mon 4:30-5:30,Fri 10:00-11:00, and by appointment",
-		typ_course:"Writing 2",
-		course_url: "",
-		email:"bafford@brandeis.edu"
-    }
+	
+    
 ];
 Template.professors.helpers({
 	professors: function(){
